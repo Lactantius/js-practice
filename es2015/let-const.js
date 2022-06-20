20f05fb3d687567308e4c127da020a5792d7ac81
Gerard Keiser <gerardkeiser@gmail.com>
2022-06-20 16:33:48 -0500

    Add let and const quiz

---
+++ es2015/let-const.js
@@ -0,0 +1,6 @@
+const PI = 3.14;
+
+// var can be redeclared and is scoped to functions, or, if global, is attached to the window object.
+// let cannot be redeclared, is scoped to blocks, and is not attached to the window object if global.
+// const is like let, but cannot be redefined (although objects bound to it may (usually) be modified)
+// Hoisting is when variable declarations are interpreted before other elements. Only works with var.
