5946ccf6a60d9928bcb2bead0ffb924a7d6a365d
Gerard Keiser <gerardkeiser@gmail.com>
2022-06-24 08:09:57 -0500

    Add gif function working

--- ajax/ajax-giphy-party/app.js
+++ ajax/ajax-giphy-party/app.js
@@ -1,1 +1,17 @@
 console.log("Let's get this party started!");
+
+const key = "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym";
+const endpoint = "https://api.giphy.com/v1/gifs/search";
+
+async function getGif(endpoint, searchString, key) {
+  const gif = await axios.get(endpoint, {
+    params: { q: searchString, limit: 1, api_key: key },
+  });
+  addGif(gif.data.data[0].images.original.url);
+}
+
+function addGif(url) {
+  const img = document.createElement("img");
+  img.src = url;
+  document.querySelector("#gif-container").append(img);
+}
