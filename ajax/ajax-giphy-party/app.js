4a50245dc3bc3c9d5a24357c272cec39e4693c4b
Gerard Keiser <gerardkeiser@gmail.com>
2022-06-24 08:34:58 -0500

    Giphy form working

--- ajax/ajax-giphy-party/app.js
+++ ajax/ajax-giphy-party/app.js
@@ -7,11 +7,27 @@ async function getGif(endpoint, searchString, key) {
   const gif = await axios.get(endpoint, {
     params: { q: searchString, limit: 1, api_key: key },
   });
-  addGif(gif.data.data[0].images.original.url);
+  insertGif(gif.data.data[0].images.original.url);
 }
 
-function addGif(url) {
+function insertGif(url) {
   const img = document.createElement("img");
   img.src = url;
   document.querySelector("#gif-container").append(img);
 }
+
+/** Search button */
+document.querySelector("#gif-form").addEventListener("submit", function (evt) {
+  evt.preventDefault();
+  const searchBox = document.querySelector("#search-box");
+  const searchString = searchBox.value;
+  getGif(endpoint, searchString, key);
+  searchBox.value = "";
+});
+
+/** Remove button */
+document
+  .querySelector("#remove-btn")
+  .addEventListener("click", () =>
+    document.querySelectorAll("img").forEach((img) => img.remove())
+  );
