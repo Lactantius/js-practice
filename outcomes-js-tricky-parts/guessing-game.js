52e2d34685a82b3a5e9f1df26f47a384317b5898
Gerard Keiser <gerardkeiser@gmail.com>
2022-09-28 10:03:02 -0500

    guessingGame passing

--- outcomes-js-tricky-parts/guessing-game.js
+++ outcomes-js-tricky-parts/guessing-game.js
@@ -1,5 +1,24 @@
 function guessingGame() {
-
+  const answer = Math.floor(Math.random() * 100);
+  let count = 0;
+  let won = false;
+  return (guess) => {
+    if (won) {
+      return "The game is over, you already won!";
+    }
+    if (guess === answer) {
+      won = true;
+      return `You win! You found ${answer} in ${count + 1} guesses.`;
+    }
+    if (guess < answer) {
+      count++;
+      return `${guess} is too low!`;
+    }
+    if (guess > answer) {
+      count++;
+      return `${guess} is too high!`;
+    }
+  };
 }
 
 module.exports = { guessingGame };
