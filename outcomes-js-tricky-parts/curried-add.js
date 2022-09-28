9669cfe3fc5a1b5ad4899b83a5fc73f2fdf6f3ae
Gerard Keiser <gerardkeiser@gmail.com>
2022-09-28 10:50:19 -0500

    curriedAdd passing

--- outcomes-js-tricky-parts/curried-add.js
+++ outcomes-js-tricky-parts/curried-add.js
@@ -1,5 +1,10 @@
-function curriedAdd(total) {
-
+function curriedAdd(n) {
+  if (typeof n === "undefined") return 0;
+  return function add(num) {
+    if (typeof num === "undefined") return n;
+    n += num;
+    return add;
+  };
 }
 
 module.exports = { curriedAdd };
